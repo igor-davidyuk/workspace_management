705f6c6c9c42ceb5b7c9262c51d2997f42037b7b
Igor-Davidyuk <igor.davidyuk@intel.com>
2023-11-03 09:33:51 +0200

    delete based on getitem

--- source/item.py
+++ source/item.py
@@ -1,24 +1,53 @@
 """Filesystem Item Module."""
+import logging
+import math
 from pathlib import Path  # Only used in `add` method
 import time
-from typing import Optional
+from typing import Dict, Optional
 import yaml
+
+def _load_type_mapping_config(path: str) -> Dict[str, str]:
+    with open(path, 'r') as reader:
+        type_mapping = yaml.safe_load(reader)
+    return type_mapping
+
+TYPE_MAPPING = _load_type_mapping_config('config/filetype_mapping.yaml')
+INDENT_SYMBOL = '  '
+FILESIZE_LITERALS = ['B', 'kB', 'MB', 'GB', 'TB']
 
 class Item:
     def __init__(self, name: str) -> None:
         self.name = name
         self.initialization_time = time.asctime()
 
+    # def view(self):
+    #     raise NotImplementedError()
+
 class File(Item):
     def __init__(self, name: str, size_b: int) -> None:
         super().__init__(name)
         self.size_b = size_b
+        # Convert size to a readable format
+        exponent = math.floor(math.log(size_b) / math.log(1024)) if size_b else 0
+        self.readable_size = str(int(size_b / 1024**exponent)) + f'{FILESIZE_LITERALS[exponent]}'
+        # Determine data type
         self.suffix = name.split('.')[-1]
+        if self.suffix not in TYPE_MAPPING:
+            # logging.warning(f'Unexpected file extantion {self.suffix}, please update the config')
+            self.type = 'file'
+        else:
+            self.type = TYPE_MAPPING[self.suffix]
+
+    def __repr__(self) -> str:
+        return f'{self.name}  ({self.readable_size}, {self.type})'
+
+    def view(self, indent: int = 0) -> str:
+        return self.indent_symbol * indent + f''
 
 class Folder(Item):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.children_dict = {}
+        self.children_dict: Dict[str, self.__class__] = {}
 
     def add(
             self, path: str,
@@ -52,26 +81,38 @@ class Folder(Item):
                 child_folder.add(str(element), ignore_hidden=ignore_hidden)
             self.children_dict[item_name] = child_folder
 
-    def _remove_item(self, name_chain: list[str], chain_index: int) -> bool:
+    def _get_item(self, name_chain: list[str], chain_index: int) -> Optional[Item]:
         if name_chain[chain_index] in self.children_dict:
             if len(name_chain) - 1 == chain_index:
-                del self.children_dict[name_chain[-1]]
-                return True
+                return self.children_dict[name_chain[-1]]
             else:
-                return self.children_dict[name_chain[chain_index]]._remove_item(name_chain, chain_index + 1)
+                return self.children_dict[name_chain[chain_index]]._get_item(name_chain, chain_index + 1)
         else:
-            return False
+            return None
 
     def delete(self, path: str) -> bool:
         name_chain = path.split('/')
+        filename = name_chain[-1]
 
         # handle cases when path is provided in form `./foo/...` or `/foo/...`
         if name_chain[0] == '' or name_chain[0] == '.':
             starting_index = 1
         else:
             starting_index = 0
-        # go down recursively to the target's parent
-        return self._remove_item(name_chain, starting_index)
+
+        if len(name_chain) - starting_index == 1:
+            parent = self
+        else:
+            # go down recursively to the target's parent
+            parent = self._get_item(name_chain[:-1], starting_index)
+        # If the parent is found and it contains the file we remove it
+        if parent and filename in parent.children_dict:
+            del parent.children_dict[filename]
+            return True
+        else:
+            logging.warning('File not found')
+            return False
+
 
 
     # def __repr__(self) -> str:
